6991b7eedb6ae79fa3568b395ee7df306efc97eb
wangchao <244067166@qq.com>
2018-07-07 23:47:24 +0800

    MMall

---
+++ src/main/java/com/wangchao/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.wangchao.dao;
+
+import com.wangchao.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/wangchao/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.wangchao.dao;
+
+import com.wangchao.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
